0c657885a4e12ef5b0be82cb3e77da5219408d01
Abdulaziz Ahmed <zidobinahmed@gmail.com>
2025-07-07 14:16:54 +0300

    Update colors.lua

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -7,23 +7,23 @@ end
 
 -- This is the tokyonight theme for neovim
 return {
-    -- {
-    --     "folke/tokyonight.nvim",
-    --     config = function()
-    --         vim.cmd.colorscheme "tokyonight"
-    --         vim.cmd('hi Directory guibg=NONE')
-    --         vim.cmd('hi SignColumn guibg=NONE')
-    --         enable_transparency()
-    --     end
-    -- },
-    {
-        "Mofiqul/vscode.nvim",
-        name = 'vscode',
-        config = function()
-            vim.cmd.colorscheme "vscode"
-            vim.cmd('hi Directory guibg=NONE')
-            vim.cmd('hi SignColumn guibg=NONE')
-            enable_transparency()
-        end
-    }
+         {
+             "folke/tokyonight.nvim",
+             config = function()
+                 vim.cmd.colorscheme "tokyonight"
+                 vim.cmd('hi Directory guibg=NONE')
+                 vim.cmd('hi SignColumn guibg=NONE')
+                 enable_transparency()
+             end
+         },
+   -- {
+    --   "Mofiqul/vscode.nvim",
+    --    name = 'vscode',
+    --    config = function()
+    --        vim.cmd.colorscheme "vscode"
+    --        vim.cmd('hi Directory guibg=NONE')
+    --        vim.cmd('hi SignColumn guibg=NONE')
+    --        enable_transparency()
+    --    end
+   -- }
 }
