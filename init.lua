7cf2c91877e7a101e29aede0b4cd04fd5eb14d11
nivoric <zidobinahmed@gmail.com>
2025-10-24 21:55:55 +0300

    Enhance text wrapping settings in init.lua
    Added options for soft line wrapping and improved readability.

--- init.lua
+++ init.lua
@@ -3,6 +3,12 @@ require('config.options')
 require('config.keybinds')
 require('config.lazy')
 
--- text wrapping
-vim.opt.wrap = true
-vim.opt.linebreak = true
+-- Soft line wrapping (visual only)
+vim.opt.wrap = true          -- enable wrapping
+vim.opt.linebreak = true     -- wrap at word boundaries
+vim.opt.breakindent = true   -- preserve indent on wrapped lines
+vim.opt.showbreak = "↪ "     -- visual prefix for wrapped lines
+
+-- Optional tweaks for better readability
+vim.opt.display:append("lastline") -- show as much as possible of the last line
+vim.opt.whichwrap:append("<,>,[,]") -- allow cursor to move across wrapped lines
